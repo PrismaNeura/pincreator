91dccfb65097c049060a2502b4b690952a85cfee
PrismaNeura <neurofamilynest@gmx.de>
2026-05-30 13:07:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -181,6 +181,8 @@ app.get('/api/image', async (req, res) => {
   } catch { res.json({ url: null }); }
 });
 
+app.get('/', (req, res) => res.sendFile('landingpage.html', { root: 'public' }));
+app.get('/app', (req, res) => res.sendFile('index.html', { root: 'public' }));
 app.get('/setup', (req, res) => res.sendFile('index.html', { root: 'public' }));
 
 // Image proxy to avoid CORS issues with canvas
